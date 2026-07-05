d5c8d7a9e28fbd6c37f963423c60601c934b875c
Sudarshan P Jadhav <42689334+sudarshanpjadhav@users.noreply.github.com>
2026-07-05 11:33:34 +0530

    finggu-asf: add/update src/index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FingguApp from './FingguApp';
+import './styles/FingguStyles.css';
+
+ReactDOM.render(<FingguApp />, document.getElementById('root'));
